299a978ccbbe5a90463badf0d1b02231a3089ae4
Daniel Schien <dschien@gmail.com>
2021-10-25 07:04:43 +0100

    polish

--- fabfile.py
+++ fabfile.py
@@ -1,23 +1,23 @@
+import sys
+
 import boto3
-import yaml
 import logging
-
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-
-with open('config.yaml') as f:
-    config = yaml.load(f)
-
 from fabric import task
 
+# use loggers right from the start, rather than 'print'
+logger = logging.getLogger(__name__)
+# this will log boto output to std out
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+
+from dotenv import dotenv_values
+config = dotenv_values(".env")
 
 @task
 def create(c):
     ec2 = boto3.resource('ec2',
-                         aws_access_key_id=config['ACCESS_KEY'],
-                         aws_secret_access_key=config['SECRET_KEY'],
-                         aws_session_token=config['SESSION_TOKEN'],
-                         region_name='us-east-1'
+                         region_name='us-east-1',
+                         # pass content of config file as named args
+                         **config
                          )
 
     instances = ec2.create_instances(
@@ -38,4 +38,5 @@ def create(c):
         Resources=[iid],
         Tags=[{'Key': 'Name', 'Value': 'COMSM0072_boto_lab'}]
     )
-    return instances[0]
+
+    logger.info(instances[0])
