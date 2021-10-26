14e03911edc79e1f7a0dec84d3c6cb98220e7a33
Daniel Schien <dschien@gmail.com>
2021-10-26 16:07:21 +0100

    add function to print ip addresses

--- fabfile.py
+++ fabfile.py
@@ -1,4 +1,5 @@
 import sys
+import time
 
 import boto3
 import logging
@@ -7,7 +8,7 @@ from fabric import task
 # use loggers right from the start, rather than 'print'
 logger = logging.getLogger(__name__)
 # this will log boto output to std out
-logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
 from dotenv import dotenv_values
 
@@ -15,7 +16,8 @@ config = dotenv_values(".env")
 
 
 @task
-def create(c):
+def create(c, name='COMSM0072_boto_lab', securitygroup=None):
+
     ec2 = boto3.resource('ec2',
                          region_name='us-east-1',
                          # pass content of config file as named args
@@ -32,6 +34,8 @@ def create(c):
         Placement={
             'AvailabilityZone': 'us-east-1a',
         },
+        SecurityGroupIds=[securitygroup] if securitygroup else [],
+        KeyName='vockey'
 
     )
     iid = instances[0].id
@@ -39,7 +43,39 @@ def create(c):
     # give the instance a tag name
     ec2.create_tags(
         Resources=[iid],
-        Tags=[{'Key': 'Name', 'Value': 'COMSM0072_boto_lab'}]
+        Tags=[{'Key': 'Name', 'Value': name}]
     )
 
     logger.info(instances[0])
+
+
+@task
+def instancedetails(c, name):
+    """
+    Return an EC2 Instance
+    :return:
+    """
+    ec2 = boto3.resource('ec2', region_name='us-east-1',
+                         # pass content of config file as named args
+                         **config
+                         )
+    instances = ec2.instances.filter(
+        Filters=[{'Name': 'tag:Name', 'Values': [name]},
+                 # {'Name': 'instance-state-name', 'Values': ['running']}
+                 ])
+    # instances is an iterator
+    instances = list(instances)
+
+    if len(instances) == 0:
+        print('not existing, will create')
+        return create(c, name=name)
+    else:
+        for instance in instances:
+            if instance.state['Name'] == "running":
+                dns = instance.public_dns_name
+                internal_ip = instance.private_ip_address
+                public_ip = instance.public_ip_address
+                logger.info(
+                    f"Instance up and running at {dns} with internal ip {internal_ip}: {public_ip}: {internal_ip}")
+            else:
+                logger.warning(f"instance {instance.id} not running")
